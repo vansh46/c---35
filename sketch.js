5cae34ed6a281be966308878fc675ec51afc10a8
Vansh <sutharvansh46@gmail.com>
2021-07-14 13:59:55 +0530

    sync

--- sketch.js
+++ sketch.js
@@ -1,29 +1,45 @@
-var ball;
-
+var hypnoticBall;
+var database
 function setup(){
+    database=firebase.database();
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.shapeColor = "red";
+    var hypnoticBallPosition = database.ref("ball/position");
+    hypnoticBallPosition.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
+   
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+       writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+      writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+       writePosition(0,+1);
     }
-    drawSprites();
+drawSprites();
+
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref('ball/position').set({
+      'x':position.x+x,
+      'y':position.y+y
+    })
+  
+}
+function readPosition(data){
+    position=data.val();
+    hypnoticBall.x=position.x;
+    hypnoticBall.y=position.y;
+}
+function showError(){
+    console.log("Error in DB");
 }
